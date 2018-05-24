5cb7a37d79ec2f8ae98efd0df754e2de8187f9cb
Lakshitha Gunasekara <lakshitha.gunasekara@gmail.com>
2018-05-24 15:12:54 +0530

    changed the candle order

--- js/mqttMessage.js
+++ js/mqttMessage.js
@@ -67,14 +67,14 @@ function onMessageArrived(message) {
     var lampStatus = message.payloadString.split("");
     console.log("split:" + lampStatus[0]+"-"+lampStatus[1]+"-"+lampStatus[2]+"-"+lampStatus[3]+"-"+lampStatus[4]);
     if (lampStatus[0] == "1") {
-        document.getElementById("lamp1Place").innerHTML = '<img class = "lamp1" src="images/candle2.png" alt="">';
+        document.getElementById("lamp5Place").innerHTML = '<img class = "lamp5" src="images/candle2.png" alt="">';
     }else {
-        document.getElementById("lamp1Place").innerHTML = '<img class = "lamp1a" src="images/candle1.png" alt="">';
+        document.getElementById("lamp5Place").innerHTML = '<img class = "lamp5a" src="images/candle1.png" alt="">';
     }
     if (lampStatus[1] == "1") {
-        document.getElementById("lamp2Place").innerHTML = '<img class = "lamp2" src="images/candle2.png" alt="">';
+        document.getElementById("lamp4Place").innerHTML = '<img class = "lamp4" src="images/candle2.png" alt="">';
     }else {
-        document.getElementById("lamp2Place").innerHTML = '<img class = "lamp2a" src="images/candle1.png" alt="">';
+        document.getElementById("lamp4Place").innerHTML = '<img class = "lamp4a" src="images/candle1.png" alt="">';
     }
     if (lampStatus[2] == "1") {
         document.getElementById("lamp3Place").innerHTML = '<img class = "lamp3" src="images/candle2.png" alt="">';
@@ -82,14 +82,14 @@ function onMessageArrived(message) {
         document.getElementById("lamp3Place").innerHTML = '<img class = "lamp3a" src="images/candle1.png" alt="">';
     }
     if (lampStatus[3] == "1") {
-        document.getElementById("lamp4Place").innerHTML = '<img class = "lamp4" src="images/candle2.png" alt="">';
+        document.getElementById("lamp2Place").innerHTML = '<img class = "lamp2" src="images/candle2.png" alt="">';
     }else {
-        document.getElementById("lamp4Place").innerHTML = '<img class = "lamp4a" src="images/candle1.png" alt="">';
+        document.getElementById("lamp2Place").innerHTML = '<img class = "lamp2a" src="images/candle1.png" alt="">';
     }
     if (lampStatus[4] == "1") {
-        document.getElementById("lamp5Place").innerHTML = '<img class = "lamp5" src="images/candle2.png" alt="">';
+        document.getElementById("lamp1Place").innerHTML = '<img class = "lamp1" src="images/candle2.png" alt="">';
     }else {
-        document.getElementById("lamp5Place").innerHTML = '<img class = "lamp5a" src="images/candle1.png" alt="">';
+        document.getElementById("lamp1Place").innerHTML = '<img class = "lamp1a" src="images/candle1.png" alt="">';
     }
 }
 
